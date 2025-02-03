68a35577bffbc16dfb0ea49ebd818a6187657819
A N D R E I <72840832+OvidiuAndrei98@users.noreply.github.com>
2025-02-03 16:26:31 +0200

    Add files via upload

---
+++ app/(home)/components/navigation/MobileNav.jsx
@@ -0,0 +1,45 @@
+'use client'
+import { Button } from 'antd'
+import './MobileNav.css'
+import { useEffect, useState } from 'react'
+
+const MobileNav = () => {
+  const [scroll, setScroll] = useState(false)
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setScroll(window.scrollY > 50)
+    })
+  }, [])
+
+  const toggleMenu = () => {
+    document.querySelector('.bar').classList.toggle('animate')
+    document.querySelector('.mobile-menu').classList.toggle('active')
+  }
+
+  return (
+    <>
+      <div
+        className={`hamburger-menu ${scroll ? 'fixed' : ''}`}
+        onClick={toggleMenu}
+      >
+        <div className="bar"></div>
+        <div className="logo">EVENTIO</div>
+      </div>
+
+      <nav className="mobile-menu">
+        <ul className="homepage-nav-menu">
+          <li className="nav-item">Acasa</li>
+          <li className="nav-item">Concept</li>
+          <li className="nav-item">Cum functioneaza</li>
+          <li className="nav-item">Preutri</li>
+          <li className="nav-item">Modele</li>
+        </ul>
+        <Button className="login-button" size="large" type="primary">
+          Intra in cont
+        </Button>
+      </nav>
+    </>
+  )
+}
+
+export default MobileNav
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,16 @@
 import '../../styles/globals.css'
+import ConceptSection from './components/concept/ConceptSection'
 import HowItWorksSection from './components/howItWorks/HowItWorksSection'
 import LandingSection from './components/landing/LandingSection'
+import PricesSection from './components/prices/PricesSection'
 
 const Home = () => {
   return (
     <>
       <LandingSection />
       <HowItWorksSection />
+      <ConceptSection />
+      <PricesSection />
     </>
   )
 }
