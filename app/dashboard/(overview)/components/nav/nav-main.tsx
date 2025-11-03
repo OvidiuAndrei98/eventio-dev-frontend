709df41ec21a9853c9ace28ebf812dec09e22df5
OvidiuAndrei98 <andrei.penica@gmail.com>
2025-11-03 13:50:53 +0200

    general fixes and qol

--- app/dashboard/(overview)/components/nav/nav-main.tsx
+++ app/dashboard/(overview)/components/nav/nav-main.tsx
@@ -6,12 +6,14 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  useSidebar,
 } from '@/components/ui/sidebar';
 import { MenuItem } from './app-sidebar';
 import { useAuth } from '@/core/context/authContext';
 
 export function NavMain({ items }: { items: MenuItem[] }) {
   const user = useAuth().userDetails;
+  const { setOpenMobile } = useSidebar();
   return (
     <SidebarGroup>
       <SidebarGroupLabel>{user?.displayName}</SidebarGroupLabel>
@@ -19,9 +21,11 @@ export function NavMain({ items }: { items: MenuItem[] }) {
         {items.map((item) => (
           <SidebarMenuItem
             key={item.title + item.url}
-            onClick={() =>
-              item.onClick && item.onClick({ title: item.title, url: item.url })
-            }
+            onClick={() => {
+              setOpenMobile(false);
+              item.onClick &&
+                item.onClick({ title: item.title, url: item.url });
+            }}
           >
             <SidebarMenuButton tooltip={item.title} className="!cursor-pointer">
               {item.icon && item.icon}
