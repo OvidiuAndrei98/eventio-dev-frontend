fdc5e6d7de95c8b470ceb26536264adb8c6c928d
OvidiuAndrei98 <andrei.penica@gmail.com>
2025-12-15 12:05:11 +0200

    major app update

--- service/event/deleteEventById.ts
+++ service/event/deleteEventById.ts
@@ -9,7 +9,7 @@ export const deleteEventById = async (event: EventInstance): Promise<void> => {
       await deleteDoc(doc(db, 'templates', event.templateId));
     }
     await deleteDoc(doc(db, 'events', event.eventId));
-    await deleteEventData(event.eventId, event.userId, event.templateId);
+    await deleteEventData(event.eventId, event.userId);
   } catch (error) {
     console.error('Error deleting the event', error);
     throw error;
