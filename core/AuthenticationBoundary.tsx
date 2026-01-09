9244934f7299275bba2e9d733731700dbfd64309
OvidiuAndrei98 <andrei.penica@gmail.com>
2026-01-09 03:16:31 +0200

    feat(general): added all lading pages, updated features/prices to be dynamic, added 2 new sections

--- core/AuthenticationBoundary.tsx
+++ core/AuthenticationBoundary.tsx
@@ -28,7 +28,7 @@ import {
 // List of pages accessible to anonymous users
 // Allow anonymous access to '/', '/login', '/register', '/planner' and all its subroutes, '/invitation' and all its subroutes
 const ANONYMOUS_PAGE_REGEX =
-  /^\/($|login$|register$|planner(\/.*)?$|invitation(\/.*)?$|invitatii-digitale-nunta(\/.*)?$|modele(\/.*)?$)/;
+  /^\/($|login$|register$|planner(\/.*)?$|invitation(\/.*)?$|invitatii-digitale-nunta(\/.*)?$|modele(\/.*)?$|invitatii-digitale-majorat(\/.*)?$|invitatii-digitale-botez(\/.*)?$)/;
 
 function isAnonymousPage(path: string) {
   return ANONYMOUS_PAGE_REGEX.test(path);
