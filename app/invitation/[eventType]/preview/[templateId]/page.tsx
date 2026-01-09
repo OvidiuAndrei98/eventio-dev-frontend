975dcec0de5e2051ccd6167253ff615eb34ea875
OvidiuAndrei98 <andrei.penica@gmail.com>
2026-01-09 13:36:29 +0200

    fix(general): fixed mockup preview page

--- app/invitation/[eventType]/preview/[templateId]/page.tsx
+++ app/invitation/[eventType]/preview/[templateId]/page.tsx
@@ -2,9 +2,10 @@
 
 import { defaultTemplates } from '@/lib/templates/templates';
 import { notFound, useParams, useRouter } from 'next/navigation';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'antd';
 import TemplateRenderer from '@/lib/templates/templateRenderer/TemplateRenderer';
+import { Template } from '@/core/types';
 
 const InvitationPreviewPage = () => {
   const { eventType, templateId } = useParams<{
@@ -16,6 +17,8 @@ const InvitationPreviewPage = () => {
   // >('mobile');
   const [innerWidth, setInnerWidth] = useState(0);
   const router = useRouter();
+  const [template, setTemplate] = useState<Template>({} as Template);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (typeof window == 'undefined') {
@@ -36,19 +39,24 @@ const InvitationPreviewPage = () => {
   //   setEditViewMode(e.target.value);
   // };
 
-  const selectedTemplate = useMemo(() => {
-    return defaultTemplates.find(
-      (template) =>
-        template.templateId === templateId && template.type === eventType
+  useEffect(() => {
+    const selectedTemplate = defaultTemplates.find(
+      (template) => template.templateId === templateId
     );
-  }, [eventType, templateId]);
+    setTemplate(selectedTemplate || ({} as Template));
+    setLoading(false);
+  }, [templateId]);
 
-  if (!selectedTemplate) {
+  if (!template) {
     notFound();
   }
 
-  return (
-    <div className="h-[100dvh] w-full">
+  return loading ? (
+    <div className="flex items-center justify-center w-full h-screen bg-[#F1F5F9]">
+      <span className="loader"></span>
+    </div>
+  ) : (
+    <div className="w-full h-[100dvh] overflow-y-[auto] w-full mx-auto">
       {/* <div className="w-full h-[60px] bg-white flex justify-center items-center border-b border-gray-200 fixed top-0 left-0 right-0 z-10">
         <Radio.Group
           size="middle"
@@ -63,11 +71,13 @@ const InvitationPreviewPage = () => {
         </Radio.Group>
       </div> */}
 
-      <TemplateRenderer
-        invitationData={selectedTemplate}
-        editMode={false}
-        previewMode={true}
-      />
+      <div className="overflow-y-auto h-full mx-auto relative">
+        <TemplateRenderer
+          invitationData={template}
+          editMode={false}
+          previewMode={true}
+        />
+      </div>
       <div className="z-[9999] bg-black text-white p-4 absolute bottom-5 left-5 flex justify-between items-center rounded-md">
         {innerWidth > 767 && <span>Model selectat: {templateId}</span>}
       </div>
